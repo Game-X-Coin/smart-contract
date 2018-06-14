ec408e7bf89785ccbf75d75e44953e4b0440b879
jindev <bjkim.dev@gmail.com>
2018-06-14 16:24:39 +0900

    fix: add min, max capped for user.

--- migrations/2_GXCCrowdsale.js
+++ migrations/2_GXCCrowdsale.js
@@ -8,11 +8,13 @@ const RATE = new web3.BigNumber(10);
 const wallet = web3.eth.accounts[9];
 const cap = new web3.BigNumber(web3.toWei(1, 'ether'));
 const goal = new web3.BigNumber(web3.toWei(100, 'ether'));
+const userMinCappedLimit = new web3.BigNumber(web3.toWei(0.1, 'ether'));
+const userMaxCappedLimit = new web3.BigNumber(web3.toWei(100, 'ether'));
 
 module.exports = async (deployer) => {
   try {
     await deployer.deploy(GXCToken);
-    await deployer.deploy(GXCSaleBase, RATE, wallet, cap, goal, openingTime, closingTime, releaseTime, GXCToken.address);
+    await deployer.deploy(GXCSaleBase, RATE, wallet, cap, goal, openingTime, closingTime, releaseTime, GXCToken.address, userMinCappedLimit, userMaxCappedLimit);
   } catch (err) {
     console.log(err);
   }
